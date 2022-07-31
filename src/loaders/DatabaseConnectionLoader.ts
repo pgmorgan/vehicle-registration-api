70212a23d30530da6995aae0e91ac7be3bf084cb
Peter Morgan <petergm@gmail.com>
2022-07-30 21:21:44 -0400

    chore: Remove unneeded files

--- src/loaders/DatabaseConnectionLoader.ts
+++
@@ -1,9 +0,0 @@
-import { Connection, createConnection } from "typeorm";
-
-export async function DatabaseConnectionLoader (
-): Promise<Connection> {
-  const connection: Connection = await createConnection();
-  console.log("[database] connected", connection.name);
-
-  return connection;
-}
--- src/loaders/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./ExpressServerLoader";
-export * from "./DatabaseConnectionLoader";
